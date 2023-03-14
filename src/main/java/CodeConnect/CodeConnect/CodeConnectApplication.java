fcda884aa13c0e3a4e9552f1f826f0caf941d9e8
junhyun1001 <jjunhyun1001@naver.com>
2023-03-14 15:04:32 +0900

    프로젝트 생성

---
+++ src/main/java/CodeConnect/CodeConnect/CodeConnectApplication.java
@@ -0,0 +1,13 @@
+package CodeConnect.CodeConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CodeConnect/CodeConnect/CodeConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package CodeConnect.CodeConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
