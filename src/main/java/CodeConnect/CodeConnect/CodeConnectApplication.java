99a7fb6b8f79ae3f629d3a5fda0bfd8645c5bff2
junhyun <junhyun1001@gmail.com>
2023-03-24 21:51:49 +0900

    react 연결 설정

--- src/main/java/CodeConnect/CodeConnect/CodeConnectApplication.java
+++ src/main/java/CodeConnect/CodeConnect/CodeConnectApplication.java
@@ -1,13 +1,28 @@
 package CodeConnect.CodeConnect;
 
+import CodeConnect.CodeConnect.domain.Member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CodeConnectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodeConnectApplication.class, args);
+	}
+
+	// react 연결 설정
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOriginPatterns();
+			}
+		};
 	}
 
 }
