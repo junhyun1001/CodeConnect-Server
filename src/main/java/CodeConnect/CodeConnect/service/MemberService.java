ff1f8e50a303d41faaa8a4ed0b89987d47990ba4
junhyun <junhyun1001@gmail.com>
2023-03-24 22:47:17 +0900

    Email 중복 검사

--- src/main/java/CodeConnect/CodeConnect/service/MemberService.java
+++ src/main/java/CodeConnect/CodeConnect/service/MemberService.java
@@ -10,6 +10,8 @@ import CodeConnect.CodeConnect.security.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class MemberService {
@@ -17,14 +19,16 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final TokenProvider tokenProvider;
 
-    // 회원가입 서비스
+    // 회원가입
     public ResponseDto<?> signUp(SignUpRequestDto dto) {
         String password = dto.getPassword();
         String passwordCheck = dto.getPasswordCheck();
 
+        validateDuplicateMember(dto.getEmail()); // 이메일 중복 체크
+
         // 비밀번호 2중 체크 값이 다를 때
         if(!password.equals(passwordCheck))
-            return ResponseDto.setFail("password does not matched");
+            return ResponseDto.setFail("Password does not match");
 
         // 클라이언트에서 받아온 값으로 Member 객체 생성
         Member member = new Member(dto);
@@ -34,14 +38,21 @@ public class MemberService {
 
     }
 
-    // 로그인 서비스
+    private void validateDuplicateMember(String email) {
+        Optional<Member> result = memberRepository.findById(email);
+        if(result.isPresent()) {
+            throw new IllegalStateException("Email already exists");
+        }
+    }
+
+    // 로그인
     public ResponseDto<SignInResponseDto> signIn(SignInRequestDto dto) {
         String email = dto.getEmail();
         String password = dto.getPassword();
         boolean findMember = memberRepository.existsByEmailAndPassword(email, password);
 
         if(!findMember)
-            return ResponseDto.setFail("Information does not match");
+            return ResponseDto.setFail("Email and Password does not match");
 
         Member member = memberRepository.findById(email).get(); // Jparepository를 구현한 memberRepository의 findById() 메소드는 Optional이기 때문에 get() 메소드를 통해 값을 불러올 수 있음
         member.setPassword("");
