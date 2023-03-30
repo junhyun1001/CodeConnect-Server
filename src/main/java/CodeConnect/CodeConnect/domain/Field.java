676d19037ad4a430e46f3964023d9bb7cec07dd7
junhyun <junhyun1001@gmail.com>
2023-03-30 14:28:26 +0900

    Field Entity 수정

--- src/main/java/CodeConnect/CodeConnect/domain/Field.java
+++ src/main/java/CodeConnect/CodeConnect/domain/Field.java
@@ -1,9 +1,35 @@
 package CodeConnect.CodeConnect.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.hibernate.mapping.Array;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * 사용자 관심 분야
  */
 
-public enum Field {
-    ANDROID, IOS, ALGORITHM, DATABASE, OS, SERVER, WEB, MACHINE_LEARNING
+@Converter
+public class Field implements AttributeConverter<List<String>, String> {
+
+    private static final String SPLIT_CHAR = ",";
+
+    @Override
+    public String convertToDatabaseColumn(List<String> attribute) {
+        return attribute.stream().map(String::valueOf).collect(Collectors.joining(SPLIT_CHAR));
+    }
+
+    @Override
+    public List<String> convertToEntityAttribute(String dbData) {
+        return Arrays.stream(dbData.split(SPLIT_CHAR))
+                .map(String::valueOf)
+                .collect(Collectors.toList());
+    }
+
 }
