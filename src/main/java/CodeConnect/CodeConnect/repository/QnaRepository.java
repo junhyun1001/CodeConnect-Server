b1d3f64fa859936fd2001b48ac2f823c5d78261d
junhyun <junhyun1001@gmail.com>
2023-03-31 17:16:21 +0900

    QnaRepository

---
+++ src/main/java/CodeConnect/CodeConnect/repository/QnaRepository.java
@@ -0,0 +1,8 @@
+package CodeConnect.CodeConnect.repository;
+
+import CodeConnect.CodeConnect.domain.post.Qna;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QnaRepository extends JpaRepository<Qna, Long> {
+
+}
