57d58c1106542d18ae53645a984341c063569303
junhyun <junhyun1001@gmail.com>
2023-03-31 17:16:39 +0900

    RecruitmentRepository

--- src/main/java/CodeConnect/CodeConnect/repository/RecruitmentRepository.java
+++ src/main/java/CodeConnect/CodeConnect/repository/RecruitmentRepository.java
@@ -1,20 +1,18 @@
 package CodeConnect.CodeConnect.repository;
 
 import CodeConnect.CodeConnect.domain.post.Post;
+import CodeConnect.CodeConnect.domain.post.Recruitment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
 
 //    @Query("SELECT p FROM Post p WHERE p.address = :address AND p.fieldList IN :fieldList")
 //    List<Post> findByAddressAndFieldList(@Param("address") String address, @Param("fieldList") List<String> fieldList);
 
-    List<Post> findByAddress(String address);
-
+    List<Recruitment> findByAddress(String address);
 
 }
