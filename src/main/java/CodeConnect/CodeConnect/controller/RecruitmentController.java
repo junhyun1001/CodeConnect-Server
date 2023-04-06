e3e004446a4f918bf7c67c6a0a0e2cba189b29e9
junhyun <junhyun1001@gmail.com>
2023-04-07 00:02:02 +0900

    게시글 쓰기, 조회, 수정, 삭제 controller 추가 및 api 주소 수정

--- src/main/java/CodeConnect/CodeConnect/controller/RecruitmentController.java
+++ src/main/java/CodeConnect/CodeConnect/controller/RecruitmentController.java
@@ -1,6 +1,5 @@
 package CodeConnect.CodeConnect.controller;
 
-
 import CodeConnect.CodeConnect.domain.post.Recruitment;
 import CodeConnect.CodeConnect.dto.ResponseDto;
 import CodeConnect.CodeConnect.dto.post.recruitment.CreateRecruitmentDto;
@@ -10,8 +9,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/recruitment")
+@RequestMapping("/recruitments")
 @RequiredArgsConstructor
 public class RecruitmentController {
 
@@ -23,12 +24,23 @@ public class RecruitmentController {
         return recruitmentService.createPost(createRequestDto, email);
     }
 
-    // 게시글 수정
-//    @PutMapping("/edit")
-//    public ResponseDto<Recruitment> editPost(@RequestBody EditRecruitmentDto editRequestDto, @AuthenticationPrincipal String email) {
-//        return recruitmentService.editPost(editRequestDto, email);
-//    }
+    // 게시글 전체 조회
+    @GetMapping("/list")
+    public ResponseDto<List<Recruitment>> getPosts(@AuthenticationPrincipal String email) {
+        return recruitmentService.getAllPosts(email);
+    }
+
+    // 게시글 수정 -> 게시글 id를 가지고 찾아야됨 (http://localhost:8080/recruitment/edit/1) 이렇게 주소 주면 됨
+    // 클라이언트에서 해당 게시글의 id를 주소에 넣어야됨
+    @PutMapping("/edit/{id}")
+    public ResponseDto<Recruitment> editPost(@RequestBody EditRecruitmentDto editRequestDto, @PathVariable Long id, @AuthenticationPrincipal String email) {
+        return recruitmentService.editPost(editRequestDto, id, email);
+    }
 
     // 게시글 삭제
+    @DeleteMapping("/delete/{id}")
+    public ResponseDto<?> deletePost(@AuthenticationPrincipal String email, @PathVariable Long id) {
+        return recruitmentService.deletePost(email, id);
+    }
 
 }
