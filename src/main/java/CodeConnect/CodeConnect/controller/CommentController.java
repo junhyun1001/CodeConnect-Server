f2f14738fdd0cd8b29bd04db8fcc10981c1f8a1f
junhyun <junhyun1001@gmail.com>
2023-05-04 02:16:52 +0900

    api 변경

--- src/main/java/CodeConnect/CodeConnect/controller/CommentController.java
+++ src/main/java/CodeConnect/CodeConnect/controller/CommentController.java
@@ -16,25 +16,29 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
+
     //댓글 생성
-    @PostMapping("/{qnaId}/create")
+    @PostMapping("/create/{qnaId}")
     public ResponseDto<Comment> createComment(@PathVariable("qnaId") Long qnaId, @RequestBody CommentRequestDto requestDto, @AuthenticationPrincipal String email) {
-        return commentService.createComment(qnaId,requestDto,email);
+        return commentService.createComment(qnaId, requestDto, email);
     }
+
     // 댓글 조회(front에서 해당 qna에 대한 댓글표시 눌렀을 때
-    @GetMapping("/{qnaId}/detail")
-    public ResponseDto<List<CommentResponseDto>> detailComment(@PathVariable("qnaId") Long qnaId,@AuthenticationPrincipal String email){
-        return commentService.findComment(qnaId,email);
+    @GetMapping("/detail/{qnaId}")
+    public ResponseDto<List<CommentResponseDto>> detailComment(@PathVariable("qnaId") Long qnaId, @AuthenticationPrincipal String email) {
+        return commentService.findComment(qnaId, email);
     }
+
     //댓글 삭제
-    @DeleteMapping("/{commentId}/delete")
-    public ResponseDto<?> deleteComment(@PathVariable("commentId") Long commentId, @AuthenticationPrincipal String email){
-        return commentService.deleteComment(commentId,email);
+    @DeleteMapping("/delete/{commentId}")
+    public ResponseDto<?> deleteComment(@PathVariable("commentId") Long commentId, @AuthenticationPrincipal String email) {
+        return commentService.deleteComment(commentId, email);
     }
+
     //댓글 업데이트
-    @PutMapping("/{commentId}/update")
-    public ResponseDto<?> updateComment(@PathVariable("commentId") Long commentId,@RequestBody CommentResponseDto commentResponseDto, @AuthenticationPrincipal String email){
-        return commentService.updateComment(commentId,email, commentResponseDto.getComment());
+    @PutMapping("/update/{commentId}")
+    public ResponseDto<?> updateComment(@PathVariable("commentId") Long commentId, @RequestBody CommentResponseDto commentResponseDto, @AuthenticationPrincipal String email) {
+        return commentService.updateComment(commentId, email, commentResponseDto.getComment());
     }
 
 
