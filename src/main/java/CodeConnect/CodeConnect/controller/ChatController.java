bfe4b8483bdd39d6531eca10d1ed45b25e42568c
junhyun <junhyun1001@gmail.com>
2023-05-22 01:25:31 +0900

    ChatDto -> ChatRequestDto

--- src/main/java/CodeConnect/CodeConnect/controller/ChatController.java
+++ src/main/java/CodeConnect/CodeConnect/controller/ChatController.java
@@ -1,16 +1,14 @@
 package CodeConnect.CodeConnect.controller;
 
-import CodeConnect.CodeConnect.dto.chat.ChatDto;
+import CodeConnect.CodeConnect.dto.chat.ChatRequestDto;
 import CodeConnect.CodeConnect.service.ChatService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @RequiredArgsConstructor
-@Slf4j
 public class ChatController {
 
     private final SimpMessagingTemplate template; // 특정 Broker로 메세지 전달
@@ -20,19 +18,15 @@ public class ChatController {
     //stompConfig에서 설정한 applicationDestinationPrefixes와 @MessageMapping 경로가 병합됨
     //"/pub/chat/enter"
     @MessageMapping("/chat/enter")
-    public void enter(ChatDto chatDto) {
-        chatDto.setMessage(chatDto.getNickname() + "님이 채팅방에 참여하였습니다.");
-        template.convertAndSend("/sub/chat/room/" + chatDto.getRoomId(), chatDto);
+    public void enter(ChatRequestDto chatRequestDto) {
+        chatRequestDto.setMessage(chatRequestDto.getNickname() + "님이 채팅방에 참여하였습니다.");
+        template.convertAndSend("/sub/chat/room/" + chatRequestDto.getRoomId(), chatRequestDto);
     }
 
     @MessageMapping("/chat/message")
-    public void message(ChatDto chatDto) {
-        log.info("채팅");
-        log.info("roomId:{}", chatDto.getRoomId());
-        log.info("nickname:{}", chatDto.getNickname());
-        log.info("message:{}", chatDto.getMessage());
-        chatService.saveChat(chatDto);
-        template.convertAndSend("/sub/chat/room/" + chatDto.getRoomId(), chatDto);
+    public void message(ChatRequestDto chatRequestDto) {
+        template.convertAndSend("/sub/chat/room/" + chatRequestDto.getRoomId(), chatRequestDto);
+        chatService.saveChat(chatRequestDto);
     }
 
 }
