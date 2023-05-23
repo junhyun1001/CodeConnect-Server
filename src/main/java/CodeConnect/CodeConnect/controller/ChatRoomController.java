63492672c3f0c63143044f2edc8a739fe19afd05
junhyun <junhyun1001@gmail.com>
2023-05-24 02:15:38 +0900

    채팅방 나가기 기능 추가

--- src/main/java/CodeConnect/CodeConnect/controller/ChatRoomController.java
+++ src/main/java/CodeConnect/CodeConnect/controller/ChatRoomController.java
@@ -5,10 +5,7 @@ import CodeConnect.CodeConnect.dto.chat.ChatRoomDto;
 import CodeConnect.CodeConnect.service.ChatRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -30,6 +27,12 @@ public class ChatRoomController {
     @GetMapping("/{id}")
     public ResponseDto<Map<String, Object>> getChatRoom(@AuthenticationPrincipal String email, @PathVariable Long id) {
         return chatRoomService.getChatRoom(email, id);
+    }
+
+    // 채팅방 나가기
+    @DeleteMapping("/{id}")
+    public ResponseDto<?> leaveChatRoom(@AuthenticationPrincipal String email, @PathVariable Long id) {
+        return chatRoomService.leaveChatRoom(email, id);
     }
 
 }
