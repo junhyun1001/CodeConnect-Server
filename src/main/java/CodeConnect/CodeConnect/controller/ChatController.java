9a96f3f583209d2030f0e2df3ab1fb52e4c06ffc
junhyun <junhyun1001@gmail.com>
2023-05-24 02:17:13 +0900

    stomp enter 메소드 삭제

--- src/main/java/CodeConnect/CodeConnect/controller/ChatController.java
+++ src/main/java/CodeConnect/CodeConnect/controller/ChatController.java
@@ -1,14 +1,11 @@
 package CodeConnect.CodeConnect.controller;
 
 import CodeConnect.CodeConnect.dto.chat.ChatRequestDto;
-import CodeConnect.CodeConnect.dto.chat.ChatResponseDto;
 import CodeConnect.CodeConnect.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,13 +16,7 @@ public class ChatController {
 
     //Client가 SEND할 수 있는 경로
     //stompConfig에서 설정한 applicationDestinationPrefixes와 @MessageMapping 경로가 병합됨
-    //"/pub/chat/enter"
-    @MessageMapping("/chat/enter")
-    public void enter(Long id) {
-//        chatRequestDto.setMessage(chatRequestDto.getNickname() + "님이 채팅방에 참여하였습니다.");
-        List<ChatResponseDto> chatList = chatService.getChatList(id);
-        template.convertAndSend("/sub/chat/room/" + id, chatList);
-    }
+    //"/pub/chat/message"
 
     @MessageMapping("/chat/message")
     public void message(ChatRequestDto chatRequestDto) {
