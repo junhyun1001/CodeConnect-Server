1eea6a8a288ca3d047f538ffadf88c8d415b09f6
junhyun <junhyun1001@gmail.com>
2023-05-28 15:09:21 +0900

    WebSocket 사용

--- src/main/java/CodeConnect/CodeConnect/controller/TodoController.java
+++ src/main/java/CodeConnect/CodeConnect/controller/TodoController.java
@@ -1,17 +1,13 @@
 package CodeConnect.CodeConnect.controller;
 
-import CodeConnect.CodeConnect.dto.ResponseDto;
 import CodeConnect.CodeConnect.dto.todo.CreateTodoRequestDto;
 import CodeConnect.CodeConnect.dto.todo.TodoResponseDto;
-import CodeConnect.CodeConnect.dto.todo.RoomIdRequestDto;
 import CodeConnect.CodeConnect.dto.todo.UpdateTodoRequestDto;
 import CodeConnect.CodeConnect.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,21 +15,6 @@ public class TodoController {
 
     private final SimpMessagingTemplate template;
     private final TodoService todoService;
-
-//    @PostMapping("/create")
-//    public ResponseDto<TodoResponseDto> createTodo(@RequestBody CreateTodoRequestDto createTodoRequestDto) {
-//        return todoService.createTodo(createTodoRequestDto);
-//    }
-//
-//    @GetMapping("/list")
-//    public ResponseDto<List<TodoResponseDto>> getTodoList(@RequestBody RoomIdRequestDto roomIdRequestDto) {
-//        return todoService.getTodoList(roomIdRequestDto);
-//    }
-//
-//    @PutMapping("/update")
-//    public ResponseDto<TodoResponseDto> updateTodo(@RequestBody UpdateTodoRequestDto updateTodoRequestDto) {
-//        return todoService.updateTodo(updateTodoRequestDto);
-//    }
 
     @MessageMapping("/todo/create")
     public void createTodo(CreateTodoRequestDto createTodoRequestDto) {
