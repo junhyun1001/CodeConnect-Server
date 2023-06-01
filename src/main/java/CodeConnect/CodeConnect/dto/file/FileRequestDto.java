d43694cdc541000cb291688ea77bf08547edc403
junhyun <junhyun1001@gmail.com>
2023-06-01 17:48:38 +0900

    생성자 수정

--- src/main/java/CodeConnect/CodeConnect/dto/file/FileRequestDto.java
+++ src/main/java/CodeConnect/CodeConnect/dto/file/FileRequestDto.java
@@ -1,8 +1,10 @@
 package CodeConnect.CodeConnect.dto.file;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class FileRequestDto {
 
     private String filePath;
