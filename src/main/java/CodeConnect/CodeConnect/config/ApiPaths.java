58357217436362b238c773c24bae225ef173b658
junhyun <junhyun1001@gmail.com>
2023-09-22 16:59:23 +0900

    Api path 변경

--- src/main/java/CodeConnect/CodeConnect/config/ApiPaths.java
+++ src/main/java/CodeConnect/CodeConnect/config/ApiPaths.java
@@ -2,16 +2,13 @@ package CodeConnect.CodeConnect.config;
 
 public class ApiPaths {
     public static final String[] ALLOWED_API = {
-            // 홈, 회원가입, 로그인
             "/",
             "/members/signup",
             "/members/login",
-            // swagger
             "/v3/api-docs",
-            "/configuration/ui",
-            "/swagger-resources/**",
-            "/configuration/security",
             "/swagger-ui/**",
-            "/webjars/**"
+            "/swagger-resources/**",
+            "/ws/**",
+            "/ws",
     };
 }
